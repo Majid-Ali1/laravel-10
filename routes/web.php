1d402c66d87acb20060fd730e8aff24cdd6e1fe1
Majid-Ali1 <ma8459088@gmail.com>
2023-06-15 11:52:37 +0500

    updates

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,3 +29,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::resource('products', ProductController::class);
